ff7ae592caa35a391916552bf53b0837b15fcd57
Nell Shamrell <nells@microsoft.com>
2020-12-22 12:54:21 -0800

    wip
    Signed-off-by: Nell Shamrell <nells@microsoft.com>

---
+++ mongo_seed/seed_data.sh
@@ -0,0 +1,4 @@
+mongoimport --host clearlydefined_mongo_db --db clearlydefined --collection definitions-paged --type json --file definitions/angular.json --jsonArray
+mongoimport --host clearlydefined_mongo_db --db clearlydefined --collection definitions-paged --type json --file definitions/babylon.json --jsonArray
+mongoimport --host clearlydefined_mongo_db --db clearlydefined --collection definitions-paged --type json --file definitions/flyway-maven-plugin.json --jsonArray
+mongoimport --host clearlydefined_mongo_db --db clearlydefined --collection curations --type json --file curations/387.json --jsonArray
