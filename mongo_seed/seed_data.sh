4f3dd6b7b7cfb3f74a892034268d28fb18e4b287
Nell Shamrell-Harrington <nellshamrell@gmail.com>
2021-01-14 14:14:13 -0800

    Update seed_data.sh

--- mongo_seed/seed_data.sh
+++ mongo_seed/seed_data.sh
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation and others. Licensed under the MIT license.
+# SPDX-License-Identifier: MIT
+
 mongoimport --host clearlydefined_mongo_db --db clearlydefined --collection definitions-paged --type json --file definitions/angular.json --jsonArray
 mongoimport --host clearlydefined_mongo_db --db clearlydefined --collection definitions-paged --type json --file definitions/babylon.json --jsonArray
 mongoimport --host clearlydefined_mongo_db --db clearlydefined --collection definitions-paged --type json --file definitions/flyway-maven-plugin.json --jsonArray
