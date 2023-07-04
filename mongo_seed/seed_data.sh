1b8fac4fcd5f0099a872924c9a869b6c7430c306
Qing Tomlinson <qing.tomlinson@sap.com>
2023-07-04 14:03:24 -0700

    Use mongo definition store for dev env set up
    Use mongo and definitions-paged collection for definitions store in
    dev environment set up.  This is because the trimmed mongo definition
    store (mongoTrimmed, which uses definitions-trimmed collection) works
    well as part of a definition store collection, but not on its own. In
    comparison, definitions-paged collection stores the definitions in
    entirety, and can be used as a standalone definition store.
    
    The population of the definitions-trimmed collection is kept in place
    to make this distinction between dev environment and production
    deployment more evident.  To emulate the production environment more
    closely, one can use dispatch+file+mongoTrimmed as
    DEFINITION_STORE_PROVIDER

--- mongo_seed/seed_data.sh
+++ mongo_seed/seed_data.sh
@@ -3,4 +3,6 @@
 
 mongoimport --host clearlydefined_mongo_db --db clearlydefined --collection definitions-trimmed --type json --file definitions/angular.json --jsonArray
 mongoimport --host clearlydefined_mongo_db --db clearlydefined --collection definitions-trimmed --type json --file definitions/flyway-maven-plugin.json --jsonArray
+mongoimport --host clearlydefined_mongo_db --db clearlydefined --collection definitions-paged --type json --file definitions/angular-paged.json --jsonArray
+mongoimport --host clearlydefined_mongo_db --db clearlydefined --collection definitions-paged --type json --file definitions/flyway-maven-plugin-paged.json --jsonArray
 mongoimport --host clearlydefined_mongo_db --db clearlydefined --collection curations --type json --file curations/387.json --jsonArray
